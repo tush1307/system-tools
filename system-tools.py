59dfd931f7d12611a8c3a230bc49e3701e29fb4e
AnuyogChauhan <anuyog.chauhan@gmail.com>
2017-12-06 11:46:24 +0530

    Update system-tools.py

--- system-tools.py
+++ system-tools.py
@@ -13,14 +13,16 @@ api = Api(app)
 
 class SystemToolTime(Resource):
     def get(self):
-        url = "http://" + os.environ['APP'] + "-system-time:10002/time"
+        ##url = "http://" + os.environ['APP'] + "-system-time:10002/time"
+        url = "http://" + os.environ['APP'] + "-system-time"
         resp = requests.get(url)
         return resp.json()
 
 
 class SystemToolUpTime(Resource):
     def get(self):
-        url = "http://" + os.environ['APP'] + "-system-uptime:10004/uptime"
+        ##url = "http://" + os.environ['APP'] + "-system-uptime:10004/uptime"
+        url = "http://" + os.environ['APP'] + "-system-uptime"
         resp = requests.get(url)
         return resp.json()
 
